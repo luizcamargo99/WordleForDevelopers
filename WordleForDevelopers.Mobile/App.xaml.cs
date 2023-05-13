1c1238c97da687e95c5c8c132d015f3fb34678f2
Luiz Camargo <luiz.zito@hotmail.com>
2023-05-13 16:51:04 +0100

    Adicionar arquivos de projeto.

---
+++ WordleForDevelopers.Mobile/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace WordleForDevelopers.Mobile;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new MainPage();
+	}
+}
---
+++ WordleForDevelopers.Mobile/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace WordleForDevelopers.Mobile;
+
+public partial class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+}
